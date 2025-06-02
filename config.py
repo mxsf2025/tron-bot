2c634a0e87b43485889859625de3915055fe0285
mxsf2025 <pangsf2008@gmail.com>
2025-06-02 09:29:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 BOT_TOKEN = '7411844178:AAFRuhYfTvyMUMAQDqyzf0ieir2vfugNX70'
-WEBHOOK_URL = 'https://your-render-url.onrender.com/'  # 修改为你的 Render URL
+WEBHOOK_URL = 'https://tron-bot-1bmm.onrender.com'  # 修改为你的 Render URL
